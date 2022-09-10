9a504b1792c131974e8af699bb5949110651e89f
Michał Chowaniec <micchowaniec@gmail.com>
2022-09-10 18:36:19 +0200

    Dodaj pliki projektów.

---
+++ CDictionary/KeyComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+     public class KeyComparer : IComparer<ComposedKey>
+    {
+        public int Compare(ComposedKey x, ComposedKey y)
+        {
+            int tmp = String.Compare(x.KeyA, y.KeyA);
+
+            if (tmp == 0)
+                return String.Compare(x.KeyB, y.KeyB);
+
+            return tmp;
+        }
+    }
+
---
+++ CDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        IDictionary<ComposedKey, char> dictionary = new SortedDictionary<ComposedKey, char>(new KeyComparer());
+
+        dictionary.Add(new ComposedKey("a", "4"), 'E');
+        dictionary.Add(new ComposedKey("a", "2"), 'C');
+        dictionary.Add(new ComposedKey("b", "2"), 'B');
+        dictionary.Add(new ComposedKey("c", "1"), 'A');
+        dictionary.Add(new ComposedKey("a", "0"), 'D');
+
+        foreach (KeyValuePair<ComposedKey, char> pair in dictionary)
+            Console.WriteLine(pair.Key + " " + pair.Value);
+    }
+}
